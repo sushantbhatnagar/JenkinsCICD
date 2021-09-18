7b2b6200b9d12a0e800ae20e19966223f1ba699e
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2021-09-18 15:30:42 +0530

    Added groovy format

---
+++ 01_NodeJs_DSL.groovy
@@ -0,0 +1,18 @@
+job('NodeJS example with DSL') {
+    scm {
+        git('https://github.com/sushantbhatnagar/JenkinsCICD.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Sushant Bhatnagar')
+            node / gitConfigEmail('sushantbhatnagar91@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('NodeJS') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
